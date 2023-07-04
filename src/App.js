b669a2ea4967d3d659d6d9b7c4189bfa57c21e4f
xiaoy <1103098607@qq.com>
2023-07-04 18:58:11 +0800

    init: first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import logo from '../public/PC.jpg';
 import './App.css';
+import Navigation from "./header";
+import React from "react";
+
+function Body() {
+    const sectionStyle = {
+        backgroundColor: 'rgb(246, 246, 248)',
+    };
+
+    const headerStyle = {
+        height: 0
+    };
+
+    const divStyle = {
+        backgroundImage: `url(${logo})`, // 使用 <img> 的图片作为背景图
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '800px',
+        margin: 'auto',
+    };
+
+    const sloganStyle = {
+        fontSize: '50px',
+        fontFamily: 'ui-sans-serif',
+        fontWeight: 'bold',
+    };
+
+    return (
+        <div>
+            <header>
+                <Navigation />
+            </header>
+            <section style={sectionStyle}>
+                <div style={divStyle}>
+                    <h1 style={sloganStyle}>Pet 是一个好网站</h1>
+                </div> {/* 使用背景图的 <div> */}
+            </section>
+        </div>
+    );
+}
+
+
+// <div style={sloganRegionStyle}>
+//     <span style={sloganStyle}>Pet 是一个非常好的产品</span>
+// </div>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div>
+              <Body />
+          </div>
+      </div>
   );
 }
 
---
+++ src/header.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import FliLogo from '../public/flipos_logo.png';
+
+const Navigation = () => {
+    const navStyle = {
+        listStyleType: "none",
+        margin: 0,
+        padding: 0,
+        overflow: "hidden",
+        backgroundColor: "#fff",
+    };
+
+    const NULLLeftBlockStyle = {
+        paddingLeft: "100px",
+        float: "left",
+        display: "flex",
+        alignItems: "center",
+        height: "80px",
+    };
+
+    const NULLRightBlockStyle = {
+        marginRight: "100px",
+        float: "right",
+        display: "flex",
+        alignItems: "center",
+        height: "80px",
+    }
+
+    const RightFloatLiStyle = {
+        float: "right",
+        display: "flex",
+        alignItems: "center",
+        height: "80px",
+    }
+
+    const linkStyle = {
+        display: "block",
+        color: "black",
+        textAlign: "center",
+        padding: "14px 16px",
+        textDecoration: "none",
+        fontFamily: "ui-sans-serif",
+        fontSize: "12.475px",
+        fontWeight: "bold"
+    };
+
+    const LongLiStyle = {
+        marginLeft: "100px",
+        float: "left",
+        display: "flex",
+        alignItems: "center",
+        height: "80px",
+    }
+
+    const ShortLiStyle = {
+        float: "left",
+        display: "flex",
+        alignItems: "center",
+        height: "80px",
+        marginLeft: "10px",
+        marginRight: "10px"
+    };
+
+    const handleClick = () => {
+        window.location.href = "localhost:3000";
+    };
+
+    const imgStyle = {
+        height: "24px",
+        margin: "auto",
+    };
+
+    const liStyle = {
+        float: "left",
+        display: "flex",
+        alignItems: "center",
+        height: "80px",
+    }
+
+    return (
+        <ul style={navStyle}>
+            <li style={NULLLeftBlockStyle}>
+                {/*<a style={linkStyle} href="/docs"></a>*/}
+            </li>
+            <li style={liStyle} className="pic">
+                <div>
+                    <img
+                        src={FliLogo}
+                        alt="图片描述"
+                        onClick={handleClick}
+                        style={imgStyle}
+                    />
+                </div>
+            </li>
+            <li style={LongLiStyle}>
+                <a style={linkStyle} href="https://stackoverflow.com/questions/tagged/my-app">Why PetWeb</a>
+            </li>
+            <li style={ShortLiStyle}>
+                <a style={linkStyle} href="https://github.com/my-app">
+                    合作商户
+                </a>
+            </li>
+            <li style={ShortLiStyle}>
+                <a style={linkStyle} href="https://github.com/my-app">
+                    产品
+                </a>
+            </li>
+            <li style={ShortLiStyle}>
+                <a style={linkStyle} href="https://github.com/my-app">
+                    价格
+                </a>
+            </li>
+            <li style={ShortLiStyle}>
+                <a style={linkStyle} href="https://github.com/my-app">
+                    联系我们
+                </a>
+            </li>
+            <li style={NULLRightBlockStyle}>
+                <a style={linkStyle} href="/docs">          </a>
+            </li>
+            <li style={RightFloatLiStyle}>
+                <a style={linkStyle} href="https://github.com/my-app">
+                    后台登录
+                </a>
+            </li>
+        </ul>
+    );
+};
+
+
+export default Navigation;
