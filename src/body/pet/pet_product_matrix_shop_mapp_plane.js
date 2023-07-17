893a9faad501ba0d7c2cb76308fbcdd06ccdd45b
xiaoy <1103098607@qq.com>
2023-07-17 19:00:29 +0800

    fix: navbar 样式调整

--- src/body/pet/pet_product_matrix_shop_mapp_plane.js
+++ src/body/pet/pet_product_matrix_shop_mapp_plane.js
@@ -2,10 +2,7 @@ import React from "react";
 import {Image} from "antd";
 import Phone1 from "../../asset/phone1.png"
 import Phone2 from "../../asset/phone2.png"
-import Phone3 from "../../asset/phone3.png"
-import Phone4 from "../../asset/phone4.png"
 import Phone5 from "../../asset/phone5.png"
-import Phone6 from "../../asset/phone6.png"
 
 class PetProductMatrixShopMappPlane extends React.Component {
     constructor(props) {
@@ -14,32 +11,37 @@ class PetProductMatrixShopMappPlane extends React.Component {
 
     render() {
         const phoneDivLsStyle = {
+            marginLeft: '10%',
+            width: '80%',
+            // padding: '20px 10%',
             display: 'flex'
         }
         const phoneDivBottomStyle = {
-            padding: '100px 70px'
+            padding: '100px 2%'
         }
         const phoneDivTopStyle = {
-            padding: '20px 70px'
+            padding: '20px 2%'
         }
-
+        const imgStyle = {
+            backgroundColor: 'rgba(235, 236, 237, 1)'
+        }
         return (
             <>
                 <div style={phoneDivLsStyle}>
                     <div style={phoneDivTopStyle}>
-                        <Image src={Phone1} alt='图片描述'/>
+                        <Image style={imgStyle} preview={false} src={Phone1} alt='图片描述'/>
                     </div>
                     <div style={phoneDivBottomStyle}>
-                        <Image src={Phone2} alt='图片描述'/>
+                        <Image style={imgStyle} preview={false} src={Phone2} alt='图片描述'/>
                     </div>
                     <div style={phoneDivTopStyle}>
-                        <Image src={Phone5} alt='图片描述'/>
+                        <Image style={imgStyle} preview={false} src={Phone5} alt='图片描述'/>
                     </div>
                     <div style={phoneDivBottomStyle}>
-                        <Image src={Phone1} alt='图片描述'/>
+                        <Image style={imgStyle} preview={false} src={Phone1} alt='图片描述'/>
                     </div>
                     <div style={phoneDivTopStyle}>
-                        <Image src={Phone1} alt='图片描述'/>
+                        <Image style={imgStyle} preview={false} src={Phone1} alt='图片描述'/>
                     </div>
                 </div>
             </>
